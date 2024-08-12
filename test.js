b437cf757ee074e1f644ad42842ae6ae0250bf05
Md Ain Uddin Ahmmad <real.world.developer@gmail.com>
2024-08-12 11:07:33 +0100

    Add files via upload

---
+++ test.js
@@ -0,0 +1,34 @@
+const board = document.querySelector('.board');
+const preGuess = document.querySelector('.preGuess');
+const hintMessage = document.querySelector('.hintMessage')
+const remainingGuess = document.querySelector('.remainingGuess')
+const displayResult = document.getElementById('displayResult')
+const p =document.createElement('BUTTON')
+const btn = document.getElementById('submitBtn')
+let playGame = true
+
+
+
+
+
+
+
+// function vaildate() {
+//   // const userInput = document.querySelector('.userInput').value
+
+//   console.log(randomNum);
+// computerGuess(randomNum)
+// }
+// vaildate()
+
+
+
+
+
+
+
+
+
+
+
+
